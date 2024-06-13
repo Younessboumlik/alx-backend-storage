129047611162a16d5539273b6ec622bc8f897e50
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-06-14 00:15:03 +0100

    task10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,17 @@
+-- Script that creates a function SafeDiv that divides the first by the second numbe
+DELIMITER $$ ;
+CREATE FUNCTION SafeDiv(
+	a INT,
+	b INT
+)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+	DECLARE result FLOAT;
+	IF b = 0 THEN
+		RETURN 0;
+        END IF;
+        SET result = (a * 1.0) / b;
+        RETURN result;
+END;$$
+DELIMITER ;
